4ca4663fa0f62d353d05db504d0f6f395856eca1
Jakob Sautner <jakob@sautner.cc>
2017-06-20 14:58:37 +0200

    offers section
    expanded, moved to tabs and filled with the right content

--- footer.php
+++ footer.php
@@ -2,26 +2,21 @@
   <div class="container">
     <div class="row grey-text text-lighten-4">
       <div class="col s12 m4 l4">
-        MAP
-      </div>
-      <div class="col s12 m4 l4">
         <div>
           <h4><i class="material-icons contact">phone</i> Telefon</h4>
           <p class="contact">+49(0)89 75 59 111</p>
         </div>
+      </div>
+      <div class="col s12 m4 l4">
         <div>
           <h4><i class="material-icons contact">mail</i> E-Mail</h4>
           <p class="contact"><a href="mailto:info@christian-gundl.de">info@christian-gundl.de</a></p>
         </div>
-        <div>
-          <h4><i class="material-icons contact">public</i> Homepage</h4>
-          <p class="contact"><a href="http://christian-gundl.de">christian-gundl.de</a></p>
-        </div>
       </div>
       <div class="col s12 m4 l4">
         <h4><i class="material-icons contact">business</i> Impressum</h4>
-        <p class="contact">Christian Gundl 	&mdash; Münchner<b>Trainer</b>Werkstatt</p>
-        <p class="contact">Tassilostr. 32<br />
+        <p class="contact">Christian Gundl &mdash; Münchner<b>Trainer</b>Werkstatt<br />
+           Tassilostr. 32<br />
            D – 82131 Gauting<br />
            UST – Id DE 130371068</p>
       </div>
@@ -29,8 +24,8 @@
   </div>
   <div class="footer-copyright">
     <div class="container">
-      &copy; 2017 - {{ "now"|date("Y") }} M&uuml;nchnerTrainerWerkstatt
-      <span class="right">Made by <a target="_blank" class="grey-text text-lighten-4" href="http://sautner.cc">Jakob Sautner</a></span>
+      &copy; 2017 - {{ "now"|date("Y") }} M&uuml;nchner<b>Trainer</b>Werkstatt
+      <span class="right">Made by <a target="_blank" class="grey-text" href="http://sautner.cc">Jakob Sautner</a></span>
     </div>
   </div>
 </footer>
