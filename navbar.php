e1fe7f204a789370ec6e5e3bef995af546ac1341
Jakob Sautner <jakob@sautner.cc>
2017-06-23 15:25:59 +0200

    minor fixes

--- navbar.php
+++ navbar.php
@@ -1,14 +1,18 @@
 <header>
-  <div class="navbar-fixed">
-    <nav>
-      <div class="nav-wrapper">
-        <a href="{{ base_url }}" class="brand-logo center"><img alt="Logo" src="{{theme_url}}/img/logo.jpg"></a>
-        <ul class="right hide-on-med-and-down">
-          {% for page in pages %}
-          <li><a href="{{ page.url }}">{{ page.title }}</a></li>
-          {% endfor %}
-        </ul>
-      </div>
-    </nav>
-  </div>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="{{ base_url }}" class="brand-logo center"><img alt="Logo" src="{{theme_url}}/img/logo.jpg"></a>
+      <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        {% for page in pages %}
+        <li><a href="{{ page.url }}">{{ page.title }}</a></li>
+        {% endfor %}
+      </ul>
+      <ul class="side-nav" id="mobile">
+        {% for page in pages %}
+        <li><a href="{{ page.url }}">{{ page.title }}</a></li>
+        {% endfor %}
+      </ul>
+    </div>
+  </nav>
 </header>
