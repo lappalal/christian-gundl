e65b15eefddfa9f7fc35b023692cebd883d43c41
Jakob Sautner <jakob@sautner.cc>
2017-08-22 16:59:45 +0200

    wip

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
   <link rel="stylesheet" href="{{ theme_url }}/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Materialize JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
